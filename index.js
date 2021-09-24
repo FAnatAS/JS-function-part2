75b0cfc75e6b70fe501dd4c5bddf8581f5c20063
FAnatAS <anatoliy.s.fedorenko@gmail.com>
2021-09-24 11:04:00 +0300

    Create variable

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-/*
-alert(20);
-alert("Hello world");
-*/ 
-// console
+//variable
+//let 
 
-console.log(1);
-console.log(undefined);
-console.log('test');
-console.log('1');
-console.log(null);
-console.log(true);
-console.log(false);
+/*
+имя переменной 
+начинается с буквы или _ или  $
+только слитно (без пробела)
+латиница
+можгут быть цифры в середние или в конце
+регистрозависимый
+ camelCase
+  отвечает на вопрос "что в ней?"
+*/
+
+//создание переменной
+let num;
+//инициализация
+num=33;
+ //логирование
+ console.log('num=', num);
+ let num2=28;
+ num=44;
+ console.log('num=', num);
+
+ const num3=77;
+ //num3=55;
+
+ {
+   let num3;
+   console.log('num2=', num2);
+   console.log('num3=', num3);
+ }
