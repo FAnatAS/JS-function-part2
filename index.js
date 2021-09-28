8fa1acff88bdd6be44986aa87a7d72c20914f4ff
FAnatAS <anatoliy.s.fedorenko@gmail.com>
2021-09-28 09:48:02 +0300

    variable+iteration

--- index.js
+++ index.js
@@ -14,3 +14,63 @@ let condition=false;
 if(condition){
   console.log(condition)
 }
+/*
+
+
+let number = 15;
+let min = 10;
+let max = 20;
+
+if(number<min || number>max){
+  console.log('You win', number)
+}
+*/
+
+let firstNumber = 45;
+let secondNumber = 12;
+let sum;
+let division;
+let multply;
+let difference;
+sum=firstNumber+secondNumber;
+console.log(sum);
+
+division=firstNumber/secondNumber;
+console.log(division);
+
+multply=firstNumber*secondNumber;
+console.log(multply);
+
+difference=firstNumber-secondNumber;
+console.log(difference);
+
+
+let anyNumber = 101;
+let logicValue = true;
+let programmingLanguage='Java';
+let teslaFounder='Elon Musk';
+let nullValue=null;
+
+
+let number=23;
+number+=11;
+console.log(number);
+
+number/=2;
+console.log(number);
+
+number--;
+console.log(number);
+
+number*=11;
+console.log(number);
+
+number++;
+console.log(number);
+
+number%=5;
+console.log(number);
+
+
+
+
