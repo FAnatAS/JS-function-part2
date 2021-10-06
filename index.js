c3dda2c37f49e60a0b1035d3188565bb8a5e525b
FAnatAS <anatoliy.s.fedorenko@gmail.com>
2021-10-06 12:07:18 +0300

    function+method

--- index.js
+++ index.js
@@ -183,23 +183,443 @@
 
 // }
 
-const calcFactorial = function (number=2) {
-  if (number<0){
-    return null;
-  }
-if(number===0 ||number===1){ //number<=1
-  return 1;
-}
-if(number>MAX_FACTORIAL){//max_factorial
-  return false;
-}
-let result=1n;
-for(let i=1; i<number;i++){
-  result*=BigInt(i);
-}
-return result;
-}
+// const calcFactorial = function (number=2) {
+//   if (number<0){
+//     return null;
+//   }
+// if(number===0 ||number===1){ //number<=1
+//   return 1;
+// }
+// if(number>MAX_FACTORIAL){//max_factorial
+//   return false;
+// }
+// let result=1n;
+// for(let i=1; i<number;i++){
+//   result*=BigInt(i);
+// }
+// return result;
+// }
 
-console.log(calcFactorial(555))//BigInt
-console.log(calcFactorial(3))//6
-console.log(calcFactorial(4))//6
+// console.log(calcFactorial(555))//BigInt
+// console.log(calcFactorial(3))//6
+// console.log(calcFactorial(4))//6
+
+
+
+/////Object
+
+// const cat={
+//   name: 'Muha',
+//   isMale: false,
+//   color:'grey',
+//   breed: 'yard',
+//   age:2,
+//   isSleeping: true,
+//   say:function(){
+//     return 'meow';
+//   },
+//   run:function(){
+
+//   },
+// };
+
+// cat.age++;
+// console.log(cat.say()); //вызов метода
+
+// cat.countLegs=4; //добавить св-во
+// console.log(cat);
+// console.log(cat.name);//вызвать св-во
+// console.log(cat.isMale);
+// console.log(cat.age);
+
+// delete cat.isSleeping;
+// console.log(cat);
+
+// // const smartPhone = {
+// //   brand: 'samsung',
+// //   color: 'red',
+// //   size: '6 inches',
+// //   memory: '6 Gb',
+// //   isWorking: true,
+// //   }
+
+// const cat2={
+//   name: 'Pushok',
+//   isMale: true,
+//   color:'white',
+//   breed: 'pers',
+//   age:4,
+//   isSleeping: true,
+//   say:function(){
+//     return 'meow';
+//   },
+//   run:function(){
+//     return 'i\'m running';
+//   },
+// };
+
+// function Cat(name, isMale, color, breed, age){ //конструктор
+//   this.name = name;
+//   this.isMale = isMale;
+//   this.color = color;
+//   this.breed = breed;
+//   this.age=age;
+//   this.say = function(){
+//     return 'meow';
+//   }
+// }
+
+// const newMuha = new Cat ('Muha', false, 'grey', 'yard', 2);
+// const kitty = new Cat ('kitty', false, 'white', 'yard', 1);
+// const catPushok = new Cat ('Pushok', true, 'black', 'yard', 4);
+
+// console.log(catPushok.name+'says '+catPushok.say());
+
+
+////////////////////
+
+
+// function User(name, sname, age){
+//   this.name = name;
+//   this.sname = sname;
+//   this.age = age;
+//   this.fullName = function(){
+//     return this.name+' '+this.sname+' '+this.age+'!!!';
+//   }
+// }
+
+// const userSmith = new User ('John', 'Smith', 22);
+
+// const userDone = new User ('Will', 'Done', 56);
+
+// const userMusk = new User ('Elon', 'Musk', 53);
+// const testUser = new User();
+
+// console.log(userSmith.name+' '+userSmith.fullName());
+
+
+
+// function Country (name, population, area){
+//   this.name = name;
+//   this.population = population;
+//   this.area = area;
+//   this.getDensity = function(){
+//     return this.population/this.area;
+//   }
+// }
+
+// const Ukraine = new Country ('Ukraine', 41362393, 603628);
+
+// // const userDone = new User ('Will', 'Done', 56);
+
+// // const userMusk = new User ('Elon', 'Musk', 53);
+// // const testUser = new User();
+
+// console.log(Ukraine.name + ' has '+ Ukraine.getDensity());
+
+// function Auto (name, maxSpeed){
+//   this.name = name;
+//   this.maxSpeed = maxSpeed;
+//   this.speed = 0;
+
+//   this.accelerate = function (value){
+//     if(value<0){
+//       return false;
+//     }
+//     this.speed+=value;
+//     if (this.speed>this.maxSpeed){
+//       this.speed=this.maxSpeed;
+//      }
+//     return this.speed;
+//   };
+//   this.decelerate = function (value){
+//     if(value<0){
+//       return false;
+//     }
+//     this.speed-=value;
+//     if (this.speed<0){
+//        this.speed=0;
+//       }
+//     return this.speed;
+
+//   };
+//  this.stop = function(){
+//    return (this.speed=0);
+//  };
+// };
+
+// const car = new Auto('bmw', 250);
+
+
+// function coffeMachine (brand, maxVolume){
+//   this.brand = brand;
+//   this.maxVolume = maxVolume;
+//   this.volume = 0;
+//   this.isOn=false;
+
+//   this.turnOn = function(){
+//     // if(this.isOn)
+//     // {
+//     //   return (this.isOn=false)
+//     // }
+//     // return (this.isOn=true);
+
+//     return (this.isOn = this.isOn ? false :true);
+
+//     //return this.isOn = !this.isOn;
+
+//     // if (value==='1'){
+//     //   this.isOn=true;
+//     //   return this.isOn;
+//     // }
+//     // else{return 'Press 1 to ON and 0 to OFF'}
+
+//     // if (value==='0'){
+//     //   this.isOn=false;
+//     //   return this.isOn;
+//     // }
+//     // else{return 'Press 1 to ON and 0 to OFF'}
+    
+//   }
+
+//   this.addWater = function (value){
+//     if(value<0 || value>this.maxVolume){
+//       return false;
+//     }
+//     if (this.isOn===false){
+//       return 'Turn on the spot';
+//     }
+//     if((this.volume+value)>this.maxVolume){
+//       return 'You can add only '+(this.maxVolume-this.volume);
+//     }
+//     this.volume+=value;
+//     // if (this.volume>this.maxVolume){
+//     //   this.volume=this.maxVolume;
+//     //  }
+//     return this.volume;
+//   };
+//   this.makeCoffe = function (value){
+//     if (this.isOn===false){
+//       return 'Turn on the spot';
+//     }
+//     if(value<0){
+//       return false;
+//     }    
+   
+//     if (value>this.volume){
+//       return 'Add water '+(value - this.volume);
+//       }
+//       this.volume-=value;
+//     return this.volume;
+
+//   };
+// //  this.stop = function(){
+// //    return (this.speed=0);
+// //  };
+// };
+
+// const machine = new coffeMachine('Saeco', 1000);
+
+// console.log('Max Volume '+machine.maxVolume);
+
+
+// const showRange = function () {
+ 
+// const min = prompt('enter min')
+// const max = prompt('enter max')
+// const number = prompt('enter div')
+
+// for (let i=max; i>=min; i--)
+// {
+//   if (i%number===0) //i%number===1 ||
+//   {
+// console.log(i);
+//   }
+
+// }
+// }
+// showRange();
+
+
+//const num1=1;
+//const num2=2;
+//const operator1='+';
+//const result=0;
+// const example = function (num1=1,num2=2, operator='+') {
+//   let result = null;
+//   //return prompt(num1+ operator1+num2);
+
+// switch (operator) {
+//       case '+':
+//         result= num1+num2;
+//         break;
+//       case '-':
+//         result= num1-num2;
+//         break;
+//       case '*':
+//         result= num1*num2;
+//         break;
+//       case '/':
+//         result= num1/num2;
+//         break;
+//       default:
+//         break;
+// }
+// if (result===null || isNaN(result))
+// {
+//   return 'error'
+// }
+
+// while (true){
+//   const question = (prompt(num1+operator+num2));
+//   if (Number(question)===result){
+//     return 'WIN!'
+//   }
+// }
+// }
+// console.log(example(45,7,'-'));
+
+
+
+
+/////////////// 05.10
+
+// const user={
+//   firstName: 'Elon',
+//   lastName:'Musk',
+//   age: 50,
+//   isMale: true,
+// }
+// const sayHello = function (obj) {
+//     return `Hello, ${obj.firstName} ${obj.lastName}!`;
+// }
+// console.log(sayHello(user));
+
+
+// const obj={
+//   name: 'pen',
+//   'is written': true,
+//   11:78,
+// }
+
+// const nameProperty='iswritten'
+// console.log(obj[11]);
+// console.log(obj['11']);
+// console.log(obj['is written']);
+// console.log(obj['name']);
+// console.log(obj[nameProperty]);
+
+/*  пример, что надо сделать. делаем ниже
+const table={
+  '12*2=':24,
+}
+*/
+
+// const obj={};
+// obj.prop1=12;
+// obj['prop 2']=22;
+
+// const createMultiplicationTable = function (min=1, max=10) {
+//   const table={};
+//   for(let i=min; i<=max; i++){
+//     for(let j=1; j<max; j++){
+//       //доступ к свойствам с возможносью вычисления. 
+//       table[`${i}*${j}=`]=i*j;
+
+//     }
+//   }
+
+//   return table;
+// }
+// const multyCount = function (str, obj) {
+//   return obj[str];
+// }
+// console.log(createMultiplicationTable())
+
+
+
+// const queryNumber=prompt('Enter here');
+// const table=createMultiplicationTable();
+
+// console.log(multyCount(queryNumber, table));
+
+// {
+//   const queryNumber='44*5=';
+//   const table=createMultiplicationTable(44,48);
+  
+//   console.log(multyCount(queryNumber, table));
+//   }
+//   {
+//     const queryNumber=prompt('Enter here');
+//     const table=createMultiplicationTable(7);
+    
+//     console.log(multyCount(queryNumber, table));
+//     }
+/*logic*/
+// function MyArrayProto() {
+//   this.push=function(item){
+//     this[this.length]=item;
+//     return ++this.length;
+//   }
+//   this.pop=function () {
+
+//     if (this.length ===0)
+//     {
+//       return;
+//     }
+
+//     const item = this[--this.length];
+//     delete this[this.length];
+//     return item;
+//   }
+// }
+// /*data*/
+// function MyArray(){
+//   this.length=0;
+// }
+// /*prototype*/
+// MyArray.prototype=new MyArrayProto();
+
+// const myArray = new MyArray();
+
+
+///// 2nd 
+
+
+
+
+
+function MyArrayProto() {
+  this.push=function(){
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length++] = arguments[i];
+    }
+    return this.length;
+  }
+  this.pop=function () {
+
+    if (this.length ===0){
+      return;
+    }
+
+    const item = this[--this.length];
+    delete this[this.length];
+    return item;
+  }
+}
+/*data*/
+function MyArray(){
+  this.length=0;
+  for (let i = 0; i < arguments.length; i++) {
+    this.push(arguments[i]);
+    
+  }
+}
+/*prototype*/
+MyArray.prototype=new MyArrayProto();
+MyArray.prototype.newMethod=function(){
+  return 'new method';
+};
+
+const myArray = new MyArray(1,1,1,15,5);
+myArray.push(2,2,2);
