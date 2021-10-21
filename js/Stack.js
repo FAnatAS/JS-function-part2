79a1ec355999fbf09812b421ae0d0b5dc343402b
FAnatAS <anatoliy.s.fedorenko@gmail.com>
2021-10-21 11:45:49 +0300

    Brackets

--- js/Stack.js
+++ js/Stack.js
@@ -33,5 +33,42 @@ class Stack{
     return lastItem;
   }
 }
-
 const stack1 = new Stack(3, 77,78,79);
+
+const options = {
+  brackets:{
+    '(':')',
+    '[':']',
+  },
+  isStrict: false,
+}
+
+/**
+ * 
+ * @param {string} str 
+ * @param {object} options 
+ * @returns 
+ */
+const checkSequence=(str, options)=>{
+  const brackets = options.brackets;  //option ['brackets'] можно так
+  const closesBrackets=Object.values(brackets);//массив всех закрывающих скобок
+  const stack = new Stack(str.length);
+  for (const symbol of str){
+    if(brackets[symbol]){ //brackets['('] true brackets['*'] undefined 
+      stack.push(symbol);
+      continue;
+    }
+    // if(stack.isEmpty){
+    //   return false;
+    // }
+    const lastBracketOfStack = stack.pick();//'[' значение по ключу
+    const correctBracket = brackets[lastBracketOfStack];
+    if(symbol===correctBracket){
+      stack.pop();
+    }else if (closesBrackets.includes(symbol)){
+      return false;
+    }
+  }
+  return stack.isEmpty;
+}
+console.log(checkSequence('8*(5+2)*(7-5)-({[2.0]}/{44}', options));
